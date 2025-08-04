fae0871c24acd564ac527a4c5b7bdb74ebcfafba
simson0524 <simson0524@naver.com>
2025-08-04 22:33:15 +0900

    Dataset 클래스 수정완료

--- dataset.py
+++ dataset.py
@@ -2,73 +2,93 @@
 
 from torch.utils.data import Dataset
 import torch
+import json
+import os
 
-LABEL_MAP = {
-    "일반": 0,
-    "개인정보": 1,
-    "기밀정보": 2
-}
+label_2_id = {"일반" : 0, "개인" : 1, "기밀" : 2, "준식별" : 3}
+id_2_label = {0 : "일반", 1 : "개인", 2 : "기밀", 3 : "준식별"}
 
 class PIISpanDataset(Dataset): # 수정 요망
-    def __init__(self, data, tokenizer, min_span_len=1, max_span_len=10, max_length=256):
-        self.data = data
+    def __init__(self, json_data, tokenizer, max_length=512):
+        self.samples = {data['id']: data for data in json_data["data"]}
+        self.annotations = {ann['id']: ann['annotations'] for ann in json_data['annotations']}
         self.tokenizer = tokenizer
-        self.min_span_len = min_span_len
-        self.max_span_len = max_span_len
+        self.label_2_id = label_2_id
         self.max_length = max_length
+        self.instances = []
+        self._create_instances()
 
-    def generate_span_candidates(self, seq_len):
-        """Generate span candidates(represented as index)
+    def _create_instances(self):
+        for sent_id, anns in self.annotations.items():
+            sent = self.samples[sent_id]["sentence"]
+            encoding = self.tokenizer(
+                sent,
+                max_length=self.max_length,
+                truncation=True,
+                return_offsets_mapping=True,
+                padding="max_length"
+            )
+            offset_mapping = encoding.pop("offset_mapping")
 
-        Args:
-            seq_len (int): Length of sequence
+            for ann in anns:
+                start, end, label = ann['start'], ann['end'], ann['label']
+                if label not in self.label_2_id:
+                    print(f"[{label}] label is not available")
+                    continue
+                label_id = self.label_2_id[label]
 
-        Returns:
-            List(tuple): Candidates of spans
-        """
-        span_candidates = []
-        for start in range( seq_len ):
-            for end in range(min(start+self.min_span_len, seq_len), min(start+self.max_span_len, seq_len)):
-                span_candidates.append( (start, end) )
-        return span_candidates
-        # 전화번호나, 주민등록번호 등 긴 번호로 나타나는 PII는 
-        # tokenize결과에 따라 너무 긴 경우 후보 span len를 길게 조정 -> 연산부담 이므로,
-        # 정규표현식 적용할 수 있는 함수 추가 및 반영방법 고민 요망 
-    
+                # span을 토큰 인덱스로 mapping
+                token_start, token_end = None, None
+                for idx, (s, e) in enumerate(offset_mapping):
+                    if s == start:
+                        token_start = idx
+                    if e == end:
+                        token_end = idx
+                
+                # span이 잘린 경우 건너뛰기
+                if token_start is None or token_end is None:
+                    print(f"Current Span is truncated. Invalid Data")
+                    continue
+
+                self.instances.append({"input_ids": encoding["input_ids"],
+                                       "attention_mask": encoding["attention_mask"],
+                                       "token_start": token_start,
+                                       "token_end": token_end,
+                                       "label": label_id,
+                                       "sentence": sent,
+                                       "span_text": sent[start:end]})
+
     def __getitem__(self, idx):
-        item = self.data[idx]
-        text, gt_spans = item['text'], set(map(tuple, item['spans'])) # 실제 라벨링 방식에 따라 수정하기 - 1
-        encoding = self.tokenizer(text, 
-                                  truncation=True, 
-                                  padding='max_length', 
-                                  max_length=self.max_length,
-                                  return_tensors='pt', 
-                                  return_offsets_mapping=True)
-        input_ids = encoding['input_ids'].squeeze(0)
-        attention_mask = encoding['attention_mask'].squeeze(0)
-        offset_mapping = encoding['offset_mapping'].squeeze(0)
-        seq_len = (attention_mask == 1).sum().item()
-
-        # Generate span candidates
-        span_candidates = self.generate_span_candidates(seq_len)
-        span_labels = [1 if span in gt_spans else 0 for span in span_candidates] # 실제 라벨링 방식에 따라 수정하기 - 2
-
-        return {"input_ids": input_ids,
-                "attention_mask": attention_mask,
-                "span_indices": span_candidates,
-                "span_labels": torch.tensor(span_labels, dtype=torch.float)}
+        item = self.instances[idx]
+        return {
+            "input_ids": torch.tensor(item["input_ids"]),
+            "attention_mask": torch.tensor(item["attention_mask"]),
+            "token_start": torch.tensor(item["token_start"]),
+            "token_end": torch.tensor(item["token_end"]),
+            "labels": torch.tensor(item["label"]),
+        }
     
     def __len__(self):
-        return len(self.data)
+        return len(self.instances)
+
+
+def load_all_json(json_dir="../Data"):
+    """Dataset 클래스에 로딩하기 위해 모든 문장별 JSON 파일의 데이터를 병합하는 함수
+
+    Args:
+        json_dir (str, optional): 문장별 JSON 파일이 있는 폴더 경로. Defaults to "../Data".
+
+    Returns:
+        dict: 모든 데이터들이 병합된 결과물
+    """
+    all_data = {"data": [],
+                "annotations": []}
     
-
-def collate_fn(batch):
-    input_ids = torch.stack([b["input_ids"] for b in batch])
-    attention_mask = torch.stack([b["attention_mask"] for b in batch])
-    span_indices = [b['span_indices'] for b in batch]
-    span_labels = [b['span_labels'] for b in batch]
-
-    return {"input_ids": input_ids,
-            "attention_mask": attention_mask,
-            "span_indices": span_indices,
-            "span_labels": span_labels}
+    for file_name in os.listdir(json_dir):
+        if file_name.endswith(".json"):
+            with open(os.path.join(json_dir, file_name), "r", encoding='utf-8') as f:
+                json_file = json.load(f)
+                all_data["data"].extend( json_file["data"] )
+                all_data["annotations"].extend( json_file["annotations"] )
+    
+    return all_data
