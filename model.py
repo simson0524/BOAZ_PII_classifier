fbe83b896ce848671549c1c8f070c6ec7ebaabad
simson0524 <simson0524@naver.com>
2025-09-21 08:10:27 +0900

    !!

--- model.py
+++ model.py
@@ -2,12 +2,22 @@
 
 import torch.nn as nn
 import torch
+import torch.nn.functional as F
+
 
 class SpanPIIClassifier(nn.Module):
-    def __init__(self, pretrained_bert, num_labels=4):
-        super().__init__()
+    def __init__(self, pretrained_bert, num_labels=4, use_focal=True, alpha=1.0, gamma=2.0, reduction="mean")        super().__init__()
         self.pretrained_bert = pretrained_bert
         self.hidden_size = pretrained_bert.config.hidden_size
+        self.num_labels = num_labels
+    
+
+        # Focal loss 관련 하이퍼파라미터
+        self.use_focal = use_focal
+        self.alpha = alpha #희귀 클래서의 주는 가중치
+        self.gamma = gamma #샘플 난이도 조절용. 정답 확률이 높은 쉬운 샘플은 무시하고, 정답 확률이 낮은 어려운 샘플은 강조하게 됨.
+        self.reduction = reduction
+        
 
         self.classifier = nn.Sequential(
             nn.Linear(self.hidden_size*2, self.hidden_size),
@@ -15,9 +25,24 @@ class SpanPIIClassifier(nn.Module):
             nn.Linear(self.hidden_size, num_labels)
         )
 
-        # TODO : Focal Loss ;;;
+    # TODO : Focal Loss ;;;
+    def focal_loss(self, logits, labels):
+        """
+        logits: (batch_size, num_labels)
+        labels: (batch_size,)
+        """
+        ce_loss = F.cross_entropy(logits, labels, reduction="none")  # 기본 CE
+        pt = torch.exp(-ce_loss)  # 확률값
+        focal_loss = self.alpha * (1 - pt) ** self.gamma * ce_loss
 
-    def forward(self, input_ids, attention_mask, token_start, token_end):
+        if self.reduction == "mean":
+            return focal_loss.mean()
+        elif self.reduction == "sum":
+            return focal_loss.sum()
+        else:
+            return focal_loss
+
+    def forward(self, input_ids, attention_mask, token_start, token_end,labels = None):
         outputs = self.pretrained_bert(
             input_ids=input_ids, # shape: (batch_size, token_len)
             attention_mask=attention_mask # shape: (batch_size, token_len)
@@ -35,4 +60,11 @@ class SpanPIIClassifier(nn.Module):
         # Classification
         logits = self.classifier(span_representation)
 
-        return {"logits": logits} 
+        loss = None
+        if labels is not None:
+            if self.use_focal:
+                loss = self.focal_loss(logits, labels)
+            else:
+                loss = F.cross_entropy(logits, labels)
+
+        return {"logits": logits, "loss": loss} 
